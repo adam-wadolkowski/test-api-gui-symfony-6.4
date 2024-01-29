5236d37786a6ba12874558279d2989016047dc56
Adam Wądołkowski <adam.wadolkowski@gmail.com>
2024-01-29 09:02:05 +0100

    Refactor api endpoint. Add command to add post.

--- app/src/Controller/Api/PostApiController.php
+++ app/src/Controller/Api/PostApiController.php
@@ -8,21 +8,21 @@ use App\Entity\Post;
 use App\ValueObject\PostVO;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
 
 #[Route('/api', name: 'api_')]
 class PostApiController extends AbstractController
 {
-/*    #[Route('/post/{$id}', name: 'get_post_by_id', requirements: ['id' => '\d+'], methods: ['get'], condition: "params['id'] > 0")]
-    #[Route('/post/{id<\d+>}', name: 'get_post_by_id', methods: ['get'], condition: "params['id'] > 0")]
-    public function index(int $id): JsonResponse
-    {
-        return $this->json(['id' => $id]);
-    }*/
-
-    #[Route('/post/{id<\d+>}', name: 'get_post_by_id', methods: ['get'])]
-    public function post(Post $post): JsonResponse
+    #[Route('/post/{id<\d+>}', name: 'get_post_by_id', methods: ['GET'])]
+    public function getPost(Post $post): JsonResponse
     {
         return $this->json((new PostVO($post))->toArray());
+    }
+
+    #[Route('/post', name: 'add_post_by_id', methods: ['POST'])]
+    public function addPost(Request $request): JsonResponse
+    {
+        dd($request->getContent());
     }
 }
