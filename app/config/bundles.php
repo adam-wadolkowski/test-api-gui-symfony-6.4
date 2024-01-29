8c2bb9ca3bc247cfa16b40ca55f3c2dd6560e701
Adam Wądołkowski <adam.wadolkowski@gmail.com>
2024-01-29 15:59:10 +0100

    Add nelmio/api-doc-bundle and symfony/asset.

--- app/config/bundles.php
+++ app/config/bundles.php
@@ -9,4 +9,5 @@ return [
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
+    Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
 ];
--- app/src/Controller/Api/PostApiController.php
+++ app/src/Controller/Api/PostApiController.php
@@ -10,11 +10,20 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
+use Swagger\Annotations as SWG;
 
 #[Route('/api', name: 'api_')]
 class PostApiController extends AbstractController
 {
     #[Route('/post/{id<\d+>}', name: 'get_post_by_id', methods: ['GET'])]
+//    #[SWG\Response(
+//        response: 200,
+//        description: "Returns the all p",
+//        #[SWG\Schema(
+//            type: "array",
+//            #[SWG\Items(ref=@Model(type=Reward::class, groups={"full"}))]
+//        ]
+//    )]
     public function getPost(Post $post): JsonResponse
     {
         return $this->json((new PostVO($post))->toArray());
